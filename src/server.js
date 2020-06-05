0c64e73ebd0e45506aba996e2d3aca6cc0a0634f
AndersonJr <andersonjunior8419@gmail.com>
2020-06-05 16:55:26 -0300

    Acresentando banco de dados SQLite

--- src/server.js
+++ src/server.js
@@ -5,12 +5,29 @@ const server = express()
 // Configurar pasta publica
 server.use(express.static("public"))
 
+
+// Ultilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+
 // Configurar caminhos da minha aplicação
 // página inicial
 // req: Requisição
 // res: Resposta
 server.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html")
+    return res.render("index.html", { title: "Um título" })
+})
+
+server.get("/create-point", (req, res) => {
+    return res.render("create-point.html")
+})
+
+server.get("/search", (req, res) => {
+    return res.render("search-results.html")
 })
 
 
